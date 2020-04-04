9c7eedfcdd6955e137869c6e2de99b30c070868b
Antonio Dell'Elce <antouk@dellelce.com>
2020-04-04 15:26:50 +0100

    add Vault makefiles

---
+++ mk/vault.mk
@@ -0,0 +1,20 @@
+#
+# This script expects the following variables:
+#      ROOT     
+VAULTHOME := $(ROOT)/vault
+VAULTDOWNLOAD := "https://www.vaultproject.io/downloads/"
+
+VAULTVERS = $$(wget -O - -q $(VAULTDOWNLOAD) | awk -f $(ROOT)/mk/vault.awk  )
+VAULTURL = https://releases.hashicorp.com/vault/$(VAULTVERS)/vault_$(VAULTVERS)_linux_amd64.zip
+
+# "shortcut" to next target
+vaultbin: $(ROOT)/bin/vault
+
+# download and "install" vault
+$(ROOT)/bin/vault:
+	@wget -q -O bin/vault.zip $(VAULTURL) && cd bin && unzip -q vault.zip && chmod +x vault && rm -f vault.zip
+
+# test target: get current version of vault
+vaultvers:
+	@echo $(VAULTVERS)
+
