726020018e23b0e7137ccb2e5001468bdfae1a38
Antonio Dell'Elce <antouk@dellelce.com>
2020-04-28 13:49:56 +0100

    add initial helm makefile: version check and "installer"

---
+++ mk/helm.mk
@@ -0,0 +1,28 @@
+#
+# This script expects the following variables:
+#      ROOT     
+HELMHOME := $(ROOT)/helm
+HELMTMP := $(ROOT)/tmp/helm-$(RANDOM)
+HELMDOWNLOAD := "https://github.com/helm/helm/releases"
+
+# We only support linux at the moment
+PLATFORM := linux-amd64
+
+HELMVERS = $$(wget -O - -q $(HELMDOWNLOAD) | awk -vext=tar.gz -f $(ROOT)/mk/github.awk  )
+HELMURL =  "https://get.helm.sh/helm-v$(HELMVERS)-$(PLATFORM).tar.gz"
+
+# $HELP$
+# helmbin                    Install current version of helm available in bin directory
+# helmvers                   Print current version of helm from official website
+
+.PHONY: helmbin
+helmbin: $(ROOT)/bin/helm
+
+# download and "install" helm
+$(ROOT)/bin/helm:
+	@mkdir $(HELMTMP) && wget -q -O $(HELMTMP)/helm.tar.gz $(HELMURL) && tar xzf $(HELMTMP)/helm.tar.gz -C $(HELMTMP) && mv $(HELMTMP)/$(PLATFORM)/helm $(ROOT)/bin/helm && rm -rf $(HELMTMP) 
+
+# get current version of helm
+.PHONY: helmvers
+helmvers:
+	@echo $(HELMVERS)
