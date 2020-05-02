41b6e2366c451fcb5d1b6f026351bd355377f417
Antonio Dell'Elce <antouk@dellelce.com>
2020-05-02 18:25:07 +0100

    add initial makefile for vagrant: install & version check

---
+++ mk/vagrant.mk
@@ -0,0 +1,24 @@
+#
+# This script expects the following variables:
+#      ROOT     
+VAGRANTHOME := $(ROOT)/vagrant
+VAGRANTDOWNLOAD := "https://www.vagrantup.com/downloads.html"
+
+VAGRANTVERS = $$(wget -O - -q $(VAGRANTDOWNLOAD) | awk -f $(ROOT)/mk/vagrant.awk  )
+VAGRANTURL = https://releases.hashicorp.com/vagrant/$(VAGRANTVERS)/vagrant_$(VAGRANTVERS)_linux_amd64.zip
+
+# $HELP$
+# vagrantbin                 Download and install vagrant in local bin directory
+# vagrantvers                Print current version from of vagrant from official website
+
+# "shortcut" to next target
+vagrantbin: $(ROOT)/bin/vagrant
+
+# download and "install" vagrant
+$(ROOT)/bin/vagrant:
+	@wget -q -O bin/vagrant.zip $(VAGRANTURL) && cd bin && unzip -q vagrant.zip && chmod +x vagrant && rm -f vagrant.zip
+
+# test target: get current version of vagrant
+vagrantvers:
+	@echo $(VAGRANTVERS)
+
