63cddd9b5c0c803bb510d12718159053acf5e37f
Antonio Dell'Elce <antouk@dellelce.com>
2020-08-04 22:02:16 +0100

    kubernetes: add coredns version

--- mk/kubernetes.mk
+++ mk/kubernetes.mk
@@ -6,10 +6,13 @@ KUBEHOME := $(ROOT)/kube
 KUBESTABLE := "https://storage.googleapis.com/kubernetes-release/release/stable.txt"
 KUBEVERS = $$(wget -O - -q $(KUBESTABLE) | sed -e 's/v//g')
 
-#etcd variables
+#etcd
 ETCDDOWNLOAD := "https://github.com/etcd-io/etcd/releases"
 ETCDVERS = $$(wget -O - -q $(ETCDDOWNLOAD) | awk -vext=tar.gz -f $(ROOT)/mk/github.awk )
 
+#coredns
+COREDNSDOWNLOAD := "https://github.com/coredns/coredns/releases"
+COREDNSVERS = $$(wget -O - -q $(COREDNSDOWNLOAD) | awk -vext=tar.gz -f $(ROOT)/mk/github.awk )
 
 # $HELP$
 # kubevers                   Print current version of kubectl from official website
@@ -19,6 +22,9 @@ kubevers:
 	@echo "kubernetes " $(KUBEVERS)
 
 # etcd targets
-
 etcdvers:
 	@echo "etcd       " $(ETCDVERS)
+
+# coredns targets
+corednsvers:
+	@echo "coredns    " $(COREDNSVERS)
